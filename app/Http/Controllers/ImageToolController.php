879c43163cdf52328c332f4fb7fbb0ffdf8e9915
M v D <dienmv@edulive.net>
2021-02-23 11:31:25 +0700

    update trim image by direction

--- app/Http/Controllers/ImageToolController.php
+++ app/Http/Controllers/ImageToolController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class ImageToolController extends Controller
 {
@@ -18,6 +19,7 @@ class ImageToolController extends Controller
         if ($image) {
             $path = $image->getRealPath();
             $name = $image->getClientOriginalName();
+            $name = Str::slug($name);
             $ext = $image->getClientOriginalExtension();
             $name = str_replace( '.' . $ext, '', $name);
 
